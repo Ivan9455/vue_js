ee8c65de20ceeb4f1810cf56ad4bf622f36f9dfe
Ivan Paderin <paderin94126@gmail.com>
2019-04-07 12:34:12 +0600

    урок 4 условия списки цыклы

---
+++ lesson4/js/main.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#app',
+    data: {
+        show: true,
+        cars:[
+            {model:"bmw",speed:250.8},
+            {model:"audi",speed:240.21},
+            {model:"mb amg",speed:350.4},
+            {model:"ford",speed:160.5}
+        ]
+    },
+    methods: {
+
+    }
+});

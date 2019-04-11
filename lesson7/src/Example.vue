fdebe01afa686a070c3fc333bedee059ce4a887c
Ivan Paderin <paderin94126@gmail.com>
2019-04-11 13:18:30 +0600

    урок 9 связь компонентов

--- lesson7/src/Example.vue
+++ lesson7/src/Example.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <h1>component example</h1>
-        <app-input></app-input>
+        <h1>{{ message }}</h1>
+        <app-input :msg="message" @messChange="message = $event"></app-input>
     </div>
 </template>
 
@@ -9,6 +9,11 @@
     import Input from './Field.vue';
 
     export default {
+        data() {
+            return {
+                message:'Пробный текст'
+            }
+        },
         components: {
             'app-input': Input
         }
